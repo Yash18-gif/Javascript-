14bb7cfb0687cf373dbd183c7dcdee7f92c1cd4b
Yash Jha <yashjha694@gmail.com>
2026-01-14 15:30:03 +0530

    Committed html file for connecting browser to javascript file

---
+++ Tutorial.js
@@ -0,0 +1,19 @@
+// const profile = {
+//     fullName:"Sharadha Khapra",
+//     followers: 480000,
+//     posts: 125,
+//     description: "Apna College | Ex- Microsoft"
+// };
+
+// console.log (profile);
+
+//Practice question
+// Get user to input a number using prompt(“Enter a number:”). Check if the number is a multiple of 5 or not.
+
+let guess = prompt("Enter a number ?");
+
+if(guess%5===0){
+    console.log("Number is multiple of 5");
+} else{
+    console.log("Number is not a multiple of 5");
+}
