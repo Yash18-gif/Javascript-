470521ec093ae92a279523ba608fd96c6de9f3e6
Yash Jha <yashjha694@gmail.com>
2026-01-20 15:26:03 +0530

    After a break

--- Tutorial.js
+++ Tutorial.js
@@ -26,15 +26,31 @@
 // 50-59, D
 // 0-49, F
 
-let grade = prompt("Enter your grade?");
-if(grade>=90 && grade<=100){
-    console.log("A");
-}else if (grade>=70 && grade<=89){
-    console.log("B");
-}else if (grade>=60 && grade<=69){
-    console.log("C");
-}else if (grade>=50 && grade<=59){
-    console.log("D");
-}else{
-    console.log("F");
-}
+// let grade = prompt("Enter your grade?");
+// if(grade>=90 && grade<=100){
+//     console.log("A");
+// }else if (grade>=70 && grade<=89){
+//     console.log("B");
+// }else if (grade>=60 && grade<=69){
+//     console.log("C");
+// }else if (grade>=50 && grade<=59){
+//     console.log("D");
+// }else{
+//     console.log("F");
+// }
+
+// Qs1. Print all even numbers from 0 to 100.
+
+// for(let e = 0;e<=100;e++){
+//     if(e%2===0){
+//         console.log(e);
+//     }
+// }
+
+// Create a game where you start with any random game number. Ask the user to keep
+// guessing the game number until the user enters correct value.
+
+let user = prompt("guessthenumber");
+let random = 37;
+
+for(us)
